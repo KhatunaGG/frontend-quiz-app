76f4e2ddb3652325a2b387c888c6cc2a2fc49d7d
Gagloshvili <KGagloshvili@oriflame.com>
2024-10-10 11:45:15 +0400

    fix

--- src/app/(pagse)/question/page.tsx
+++ src/app/(pagse)/question/page.tsx
@@ -1,9 +1,9 @@
-import QuestionSection from "@/app/components/_molecules/questionSection/QuestionSection";
+import { QuestionSection } from "@/app/components";
 
 export default function page() {
   return (
     <div className="px-[6.4%]  md:pb-[57px] md:px-[8.33%] lg:pl-[9.72%]  lg:h-[calc(100vh-224px)] ">
-      <QuestionSection />
+      <QuestionSection/>
     </div>
   );
 }
